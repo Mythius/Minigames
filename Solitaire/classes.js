d5acd9923114913006163e8f8eb5cd40f1271208
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2019-10-31 15:44:03 -0600

    Add files via upload

--- Solitaire/classes.js
+++ Solitaire/classes.js
@@ -1,8 +1,11 @@
+var VERIFIED = false;
 function Card(n,t){
 	var number = n,type = t,faceup = false,flipping = false,image = create('img');
 	image.src = getPath();
 	image.classList.add('card');
 	image.style.transform='rotateY(180deg)';
+	image.draggable = false;
+	this.mouse = false;
 	this.getImage = () => image;
 	function getPath(){
 		if(faceup){
@@ -26,6 +29,10 @@ function Card(n,t){
 	}
 	this.card = n+t;
 	this.getColor = () => t == 'D' || t == 'C' ? 'black' : 'red';
+	this.removeEvents = function(){
+		var new_element = image.cloneNode(true);
+		image.replaceWith(new_element);
+	}
 }
 
 function Deck(){
@@ -54,6 +61,7 @@ function Deck(){
 	}
 	this.display = function(){
 		for(let s of stacks){
+			s.update();
 			s.display();
 		}
 		for(let i=count;i<cards.length;i++){
@@ -64,6 +72,7 @@ function Deck(){
 }
 
 function Stack(x,c){
+	var STACK = this;
 	var cards = c;
 	var div = create('div');
 	div.classList.add('stack');
@@ -76,18 +85,53 @@ function Stack(x,c){
 		return cards[cards.length-1];
 	}
 	function removeLast(){
-
+		cards.pop();
 	}
 	function color(){
 		return getLast().getColor();
+	}
+	function getPosition(n){
+		return (n * 30) + 10;
 	}
 	this.getCards = () => cards;
 	this.display = function(){
 		for(let i=0;i<cards.length;i++){
 			let card = cards[i];
 			card.getImage().style.left = x + 'px';
-			card.getImage().style.top = (i * 30) + 10 + 'px';
-			div.appendChild(card.getImage());
+			card.getImage().style.top = getPosition(i) + 'px';
+			obj('#main').appendChild(card.getImage());
 		}
+	}
+	this.update = update;
+	function update(){
+		for(let c of cards){
+			c.removeEvents();
+		}
+		let card = getLast();
+		let last = card.getImage();
+		last.on('mousedown',function(e){
+			card.mouse = true;
+			removeLast();
+		});
+		last.on('mousemove',function(e){
+			if(card.mouse){
+				last.style.left = e.clientX - 30 + 'px';
+				last.style.top = e.clientY - 30 + 'px';
+			}
+		});
+		last.on('mouseup',function(e){
+			card.mouse = false;
+			setTimeout(()=>{
+				if(VERIFIED){
+
+				} else {
+					STACK.add([card]);
+				}
+			});
+		});
+	}
+	this.add = function(c){
+		cards.concat();
+		update();
 	}
 }
